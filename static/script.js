e84ea1d99dc10f48ab2014463b58f05f6883bd78
ProgramMen <83456250+ProgramMen@users.noreply.github.com>
2023-06-23 16:54:09 +0300

    Update script.js

--- static/script.js
+++ static/script.js
@@ -8,18 +8,30 @@ const form = document.getElementById('form');
 const input = document.getElementById('input');
 const logout = document.getElementById('logout');
 
+let url = "http://api.weatherapi.com/v1/current.json?key=b3561c41d0154b909ff135649231806&q=Kivertsi&aqi=no";
+let xhr = new XMLHttpRequest();
+xhr.responseType = "json";
+xhr.open("GET", url);
+xhr.send();
+let weather = "";
+xhr.onload = function() {
+  weather = xhr.response;
+  if (xhr.status > 400) {
+    $("#result").html("There is some error...");
+  }
+  else {
+    console.log(weather);
+    console.log(weather.current);
+    $('#icon').attr("src", "https:" + weather.current.condition.icon);
+    $('#city').html(weather.location.country + " , " + weather.location.name);
+    $('#temp').html(weather.current.temp_c + "°C");
+  }
+}
+
+
 logout.addEventListener('click', (e) => {
   document.cookie = 'token=; Max-Age=0';
   location.assign('/login');
-});
-
-socket.on('all_messages', function(msgArray) {
-  msgArray.forEach(msg => {
-    let item = document.createElement('li');
-    item.textContent = msg.login + ': ' + msg.content;
-    messages.appendChild(item);
-  });
-  window.scrollTo(0, document.body.scrollHeight);
 });
 
 form.addEventListener('submit', function(e) {
@@ -31,18 +43,17 @@ form.addEventListener('submit', function(e) {
 });
 
 socket.on('message', function(msg) {
-    let item = document.createElement('li');
+    var item = document.createElement('li');
     item.textContent = msg;
     messages.appendChild(item);
     window.scrollTo(0, document.body.scrollHeight);
 });
-let url = "https://api.weatherapi.com/v1/current.json?key=63ee929dc5124e97a8e171305231006&q=Kiev&aqi=no";
-let xhr = new XMLHttpRequest();
-xhr.responseType = "json";
-xhr.open("GET", url);
-xhr.send();
-let weth = "";
-xhr.onload = function(){
-  weth = xhr.response;
-  console.log(weth);
-};
+
+socket.on('all_messages', function(msgArray) {
+  msgArray.forEach(msg => {
+    let item = document.createElement('li');
+    item.textContent = msg.login + ': ' + msg.content;
+    messages.appendChild(item);
+  });
+  window.scrollTo(0, document.body.scrollHeight);
+});
